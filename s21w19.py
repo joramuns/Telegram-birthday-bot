9bb21e13cd4c2a103d7086531519480a29d3d03b
Vadim <joramuns@student.21-school.ru>
2023-05-29 00:20:07 +0300

    :star: feat: asyncio implementation

--- s21w19.py
+++ s21w19.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
-import telebot
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 from telebot.types import BotCommand, InlineKeyboardMarkup, InlineKeyboardButton
-import re, time
+import re, time, datetime
 import config, sqlite_bot, bd_next, bd, bd_list
 import threading
 #temp
 import random
 
-bot = telebot.TeleBot(config.token);
-bot_id = bot.get_me().id
+bot = AsyncTeleBot(config.token);
+# bot_id = bot.get_me().id
 
 @bot.message_handler(commands=["ближайшаяднюха", "birthday_next"])
-def birthday_next(message):
+async def birthday_next(message):
     output_message = bd_next.output(message.chat.id, message.from_user.id)
-    bot.send_message(message.chat.id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
+    await bot.send_message(message.chat.id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
 
 @bot.callback_query_handler(func=lambda call: True)
-def inline_keyboard(call):
+async def inline_keyboard(call):
     get_num_month = [n for n, x in enumerate(config.monthes) if call.data in x]
     output_message = bd_list.month_list(get_num_month, bot_id)
-    bot.send_message(config.manual_chat_id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
+    await bot.send_message(config.manual_chat_id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
 
 @bot.message_handler(commands=["днюхи", "birthday_list"])
-def birthday_list(message):
+async def birthday_list(message):
     options = []
     reply_markup = InlineKeyboardMarkup([options])
     for item in config.monthes:
@@ -30,30 +31,32 @@ def birthday_list(message):
         reply_markup.row(button)
 
     output_message=bd_list.output(message)
-    bot.send_message(message.chat.id, output_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id, reply_markup=reply_markup)
+    await bot.send_message(message.chat.id, output_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id, reply_markup=reply_markup)
 
 @bot.message_handler(commands=["днюха", "birthday"])
-def birthday(message):
+async def birthday(message):
     output_message = bd.output(message)
-    bot.send_message(message.chat.id, output_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id)
+    await bot.send_message(message.chat.id, output_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id)
 
 @bot.message_handler(commands=["помощь"])
-def helpmenu(message):
-    bot.send_message(message.chat.id, config.help_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id)
+async def helpmenu(message):
+    await bot.send_message(message.chat.id, config.help_message, parse_mode="HTML", reply_to_message_id=config.manual_thread_id)
 
-event_job = threading.Event()
-def job_handler():
-    while not event_job.is_set():
+async def job_handler():
+    while (1):
         days_left = bd_next.count_days(config.manual_chat_id, bot_id)
         if days_left < 3:
             output_message = bd_next.output(config.manual_chat_id, bot_id)
-            bot.send_message(config.manual_chat_id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
-        event_job.wait(43200)
+            await bot.send_message(config.manual_chat_id, output_message, reply_to_message_id=config.manual_thread_id, parse_mode="HTML")
+        await asyncio.sleep(43200)
 
-job_thread = threading.Thread(target=job_handler)
-job_thread.start()
+async def starter():
+    global bot_id
+    bot_id = (await bot.get_me()).id
+    task1 = asyncio.create_task(job_handler())
+    task2 = asyncio.create_task(bot.polling())
 
-bot.polling(none_stop=True, interval=1)
+    await task1
+    await task2
 
-event_job.set()
-job_thread.join()
+asyncio.run(starter())
